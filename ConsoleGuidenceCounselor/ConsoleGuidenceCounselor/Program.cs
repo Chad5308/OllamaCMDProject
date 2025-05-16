f1b52b76908c44bc47f787cbfca90706e0084a85
Chad Perry <chad25309@gmail.com>
2025-05-16 11:04:33 -0400

    Added in setup instructions and PA Grad requirements

--- ConsoleGuidenceCounselor/ConsoleGuidenceCounselor/Program.cs
+++ ConsoleGuidenceCounselor/ConsoleGuidenceCounselor/Program.cs
@@ -1,6 +1,7 @@
 ﻿#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 using System;
+using System.IO;
 using ConsoleGuidenceCounselor.Plugins;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
@@ -12,28 +13,23 @@ var endpoint = new Uri("http://localhost:11434");
 
 builder.Services.AddOllamaChatCompletion(modelId, endpoint);
 
-builder.Plugins
-    .AddFromType<MyTimePlugin>()
-    .AddFromObject(new MyLightPlugin(turnedOn: true))
-    .AddFromObject(new MyAlarmPlugin("11"))
-    .AddFromObject(new FileSystemPlugin());
+//builder.Plugins
+//    .AddFromType<MyTimePlugin>()
+//    .AddFromObject(new MyLightPlugin(turnedOn: true))
+//    .AddFromObject(new MyAlarmPlugin("11"))
+//    .AddFromObject(new AcademicInfoPlugin());
+builder.Plugins.AddFromObject(new AcademicInfoPlugin())
+    .AddFromObject(new GraduationRequirements());
 var kernel = builder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
 
+var path = Path.Combine(Environment.CurrentDirectory, "SetupText.txt");
+var setuptext = File.ReadAllText(path);
 
 
-
-Console.WriteLine("""
-    Ask questions or give instructions to the copilot such as:
-    - Change the alarm to 8
-    - What is the current alarm set?
-    - Is the light on?
-    - Turn the light off please.
-    - Set an alarm for 6:00 am.
-    """);
-
-Console.Write("> ");
+ChatMessageContent setupResult = await chatCompletionService.GetChatMessageContentAsync(setuptext, settings, kernel);
+Console.Write($"\n>>> Result: {setupResult}\n\n> ");
 
 string? input = null;
 while ((input = Console.ReadLine()) is not null)
